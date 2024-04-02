31cb228ab2c38d0b190f0b9c0c3f057ee45b804f
Soumia <s_aya@live.fr>
2024-04-02 09:34:16 +0200

    page le parc

--- Rooter/allRoutes.js
+++ Rooter/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "./pages/home.html"),];
+    new Route("/", "Accueil", "./pages/home.html"),
+    new Route("/leparc","Le parc","./pages/leparc.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcandia";
