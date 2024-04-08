088ea89a9171016f18dee75e0fa292c667965c6c
Soumia <s_aya@live.fr>
2024-04-08 09:17:37 +0200

    pages habitats

--- Rooter/allRoutes.js
+++ Rooter/allRoutes.js
@@ -4,6 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
     new Route("/leparc","Le parc","./pages/leparc.html"),
+    new Route("/contactclient","Contact","./pages/contactclient.html"),
+    new Route("/parkservice","Préparez votre visite","./pages/parkservice.html"),
+    new Route("/jungle","Arcandia: La jungle","./pages/jungle.html"),
+    new Route("/savane","Arcandia: La savane","./pages/savane.html"),
+    new Route("/marais","Arcandia: Les marais","./pages/marais.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
