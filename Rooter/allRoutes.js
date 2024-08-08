0370024c7be64a78553a28d01057823d51f1e6ea
Soumia <s_aya@live.fr>
2024-08-08 18:53:21 +0200

    maj page

--- Rooter/allRoutes.js
+++ Rooter/allRoutes.js
@@ -6,9 +6,11 @@ export const allRoutes = [
     new Route("/leparc","Le parc","./pages/leparc.html"),
     new Route("/contactclient","Contact","./pages/contactclient.html"),
     new Route("/parkservice","Préparez votre visite","./pages/parkservice.html"),
-    new Route("/jungle","Arcandia: La jungle","./pages/jungle.html"),
-    new Route("/savane","Arcandia: La savane","./pages/savane.html"),
-    new Route("/marais","Arcandia: Les marais","./pages/marais.html"),
+    new Route("/jungle","Arcandia: La jungle","./pages/habitats/jungle.html"),
+    new Route("/savane","Arcandia: La savane","./pages/habitats/savane.html"),
+    new Route("/marais","Arcandia: Les marais","./pages/habitats/marais.html"),
+    new Route("/connexion","Connexion","./pages/connexion.html"),
+    new Route("/account","Espace employé","./pages/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
